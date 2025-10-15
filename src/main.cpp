53cd16761f5e8fb1bcba60e7e20309a3732951eb
Jakob Stechow <jakob.stechow@pm.me>
2025-10-15 21:19:39 +0200

    feat: initial commit

---
+++ src/main.cpp
@@ -0,0 +1,108 @@
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+#include <GL/gl.h>
+#include <iostream>
+
+const char *vertexShaderSource = "#version 330 core\n"
+  "layout (location = 0) in vec3 aPos;\n"
+  "void main()\n"
+  "{\n"
+  " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+  "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+  "out vec4 FragColor;\n"
+  "void main()\n"
+  "{\n"
+  "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+  "}";
+
+void framebuffer_size_change_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) {
+  if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
+
+int main() {
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+
+  if (window == NULL) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_change_callback);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+
+  glViewport(0, 0, 800, 600);
+
+  unsigned int vertexShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+
+  int compileVertexShaderSuccess;
+  char infoLog[512];
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &compileVertexShaderSuccess);
+
+  if (!compileVertexShaderSuccess) {
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    std::cout << "ERROR: Vertex shader compilation failed!\n" << infoLog << std::endl;
+  }
+
+  unsigned int fragmentShader;
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+
+  int compileFragmentShaderSuccess;
+  char infoLogFragmentShader[512];
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &compileFragmentShaderSuccess);
+
+  if (!compileFragmentShaderSuccess) {
+    glGetShaderInfoLog(fragmentShader, 512, NULL, infoLogFragmentShader);
+    std::cout << "ERROR: Fragment shader compilation failed!\n" << infoLogFragmentShader << std::endl;
+  }
+
+  // TODO: check if shader program failed
+  unsigned int shaderProgram;
+  shaderProgram = glCreateProgram();
+  glad_glAttachShader(shaderProgram, vertexShader);
+  glad_glAttachShader(shaderProgram, fragmentShader);
+  glad_glLinkProgram(shaderProgram);
+
+  glad_glUseProgram(shaderProgram);
+
+  // we no longer need the shader objects as they are now linked in the program object
+  glad_glDeleteShader(vertexShader);
+  glad_glDeleteShader(fragmentShader);
+
+  while(!glfwWindowShouldClose(window)) {
+    processInput(window);
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
+}
